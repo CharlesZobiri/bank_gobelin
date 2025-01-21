1516a8bf5150839b504625200c1a247a37a9ce20
CharlesZobiri <czkadev@gmail.com>
2025-01-21 13:45:57 +0100

    feat : default account sold set to 0

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-
-
 from pydantic import BaseModel
 class Account(BaseModel):
-    sold: float
+    sold: float = 0.0
     userID: int
     name: str
-
 
 def isTransferPossible(amount, firstAccount):
     return (firstAccount.sold > 0 and amount <= firstAccount.sold and amount > 0)
@@ -14,9 +11,11 @@ def transferMoney(amount, firstAccount, secondAccount):
     if isTransferPossible(amount, firstAccount):
         firstAccount.sold -= amount
         secondAccount.sold += amount
+    else : 
+        print("This account isn't sold enough to make the transfer")
     return firstAccount, secondAccount
 
-firstAccount = Account(userID=1, name='testAccount', sold=1000.0)
+firstAccount = Account(userID=1, name='testAccount')
 secondAccount = Account(userID=2, name='testAccount2', sold=2000.0)
 
 firstAccount, secondAccount = transferMoney(20, firstAccount, secondAccount)
