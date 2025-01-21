697898f4a2ece9037f477d2b0c8dc5cbe0b4a5f9
CharlesZobiri <czkadev@gmail.com>
2025-01-21 13:35:29 +0100

    feat : added Account class + checking if the account is solvable

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-firstAccount = 450
-secondAccount = 500.0
 
-def transferMoney(amount, fromAccount, toAccount):
-    if(isTransferPossible(amount, fromAccount)):
-     return fromAccount - amount, toAccount + amount
-    else:
-     print("Transfer not possible")
-     return fromAccount, toAccount
 
-    
+from pydantic import BaseModel
+class Account(BaseModel):
+    sold: float
+    userID: int
+    name: str
 
-def isTransferPossible(amount, fromAccount):
-    return (fromAccount > 0 and amount <= fromAccount and amount > 0)
 
- 
-print(firstAccount, secondAccount)
+def isTransferPossible(amount, firstAccount):
+    return (firstAccount.sold > 0 and amount <= firstAccount.sold and amount > 0)
+
+def transferMoney(amount, firstAccount, secondAccount):
+    if isTransferPossible(amount, firstAccount):
+        firstAccount.sold -= amount
+        secondAccount.sold += amount
+    return firstAccount, secondAccount
+
+firstAccount = Account(userID=1, name='testAccount', sold=1000.0)
+secondAccount = Account(userID=2, name='testAccount2', sold=2000.0)
+
 firstAccount, secondAccount = transferMoney(20, firstAccount, secondAccount)
-print(firstAccount, secondAccount)
+print(f"firstAccount sold={firstAccount.sold} userID={firstAccount.userID} name='{firstAccount.name}'")
+print(f"secondAccount sold={secondAccount.sold} userID={secondAccount.userID} name='{secondAccount.name}'")
